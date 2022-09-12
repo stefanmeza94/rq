80fb00be0d62886be07dfd003a1587f60898aa87
Stefan Meza <stefanmeza93@gmail.com>
2022-09-12 16:41:20 +0200

    First query call

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,26 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
 export const RQSuperHeroesPage = () => {
-  return <div>RQ Super Heroes Page</div>;
+  const {
+    isLoading,
+    isError,
+    error,
+    data: { data: superheroes },
+  } = useQuery(['super-heroes'], () => {
+    return axios('http://localhost:4000/superheroes');
+  });
+
+  console.log(isLoading, isError, error, superheroes);
+
+  return (
+    <>
+      <div>RQ Super Heroes Page</div>
+      <ul>
+        {superheroes?.map((superhero) => {
+          return <li key={superhero.id}>{superhero.name}</li>;
+        })}
+      </ul>
+    </>
+  );
 };
