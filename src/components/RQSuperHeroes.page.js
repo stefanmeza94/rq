0d7eca053e506c6528641121e9a946a6589dfa3c
Stefan Meza <stefanmeza93@gmail.com>
2022-09-13 16:08:06 +0200

    Add cacheTime to query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -3,25 +3,32 @@ import axios from 'axios';
 
 const fetchSuperHeroes = () => {
   return axios('http://localhost:4000/superheroes');
-}
+};
 
 export const RQSuperHeroesPage = () => {
-  const { data, isLoading, isError, error } = useQuery(['super-heroes'], fetchSuperHeroes);
+  const { data, isLoading, isError, error, isFetching } = useQuery(
+    ['super-heroes'],
+    fetchSuperHeroes,
+    { cacheTime: 5000 }
+  );
+
+  console.log(isLoading, isFetching);
 
   if (isLoading) {
-    return <h2>Loading...</h2>
+    return <h2>Loading...</h2>;
   }
 
   if (isError) {
-    return <h2>Something went wrong. {error.message}</h2>
+    return <h2>Something went wrong. {error.message}</h2>;
   }
 
   return (
     <>
       <div>RQ Super Heroes Page</div>
-      <ul>{data?.data.map((superhero) => {
-        return <li key={superhero.id}>{superhero.name}</li>;
-      })}
+      <ul>
+        {data?.data.map((superhero) => {
+          return <li key={superhero.id}>{superhero.name}</li>;
+        })}
       </ul>
     </>
   );
