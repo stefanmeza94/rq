cc8b25a2878e7af9e0eb38ec9786de9d2979a931
Stefan Meza <stefanmeza93@gmail.com>
2022-09-13 09:44:19 +0200

    Add ReactQueryDevtools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import './index.css';
 import App from './App';
 
@@ -11,7 +12,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
     </QueryClientProvider>
   </React.StrictMode>
 );
-
